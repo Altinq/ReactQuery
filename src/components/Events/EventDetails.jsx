8e81da1180cac10bd523f2aee4ac94b4d7121d06
Altinq <altin.qunaku@gmail.com>
2025-07-16 14:52:40 +0200

    Handle loading and error states in EventDetails; display error block on fetch failure and loading message while fetching event data

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,11 +1,13 @@
-import { Link, Outlet, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import Header from "../Header.jsx";
-import { fetchEvent } from "../../util/http.js";
+import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
   const { id } = useParams();
+  const navigate = useNavigate();
+
   const {
     data: event,
     isPending,
@@ -15,6 +17,21 @@ export default function EventDetails() {
     queryKey: ["events", id],
     queryFn: ({ signal }) => fetchEvent({ id, signal }),
   });
+
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["events"],
+        refetchType: "none",
+      });
+      navigate("/events");
+    },
+  });
+
+  function handleDelete() {
+    mutate({ id });
+  }
 
   let content;
   if (isPending) {
@@ -43,7 +60,7 @@ export default function EventDetails() {
         <header>
           <h1>{event?.title}</h1>
           <nav>
-            <button>Delete</button>
+            <button onClick={handleDelete}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
